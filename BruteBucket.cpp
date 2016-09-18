77af5e75b0bb4ef6811627ad4da2b76637bb2681
Basti Destruction <qwertz1029384756@gmail.com>
2016-09-18 20:47:54 +0200

    init

--- BruteBucket.cpp
+++ BruteBucket.cpp
@@ -1,11 +1,222 @@
-// BruteBucket.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "BruteBucket.h"
+
+using namespace std;
+
+
+class Bucket
+{
+private:
+public:
+	int maxVol;
+	int curVol;
+	string name;
+	Bucket(int maximumVolume, int currentVolume, string bucketName);
+	void fillBucket();
+	void emptyBucket();
+	void decantBucket(Bucket & destination);
+	void print();
+};
+
+Bucket::Bucket(int maximumVolume, int currentVolume, string bucketName)
+{
+	maxVol = maximumVolume;
+	curVol = currentVolume;
+	name = bucketName;
+}
+
+void Bucket::emptyBucket()
+{
+	curVol = 0;
+}
+
+void Bucket::fillBucket()
+{
+	curVol = maxVol;
+}
+
+void Bucket::decantBucket(Bucket& destination)
+{
+	int freeDestinationSpace = destination.maxVol - destination.curVol;
+
+	// if there is space left in destination bucket
+	if (freeDestinationSpace > 0)
+	{
+		// if whole source bucket can be decanted into destination bucket without a overflow
+		if (freeDestinationSpace >= curVol)
+		{
+			destination.curVol += curVol;
+			curVol = 0;
+		}
+		// when the source bucket can only partially be decanted into the destination bucket
+		else
+		{
+			curVol -= freeDestinationSpace;
+			destination.curVol += freeDestinationSpace;
+		}
+	}
+}
+
+void Bucket::print()
+{
+	cout << name << "{" << curVol << "/" << maxVol << "}" << endl;
+}
+
+
+class BucketNode
+{
+public:
+	BucketNode *decant1, *decant2, *empty1, *empty2, *fill1, *fill2, *parent;
+	Bucket *one, *two;
+	int depth;
+	BucketNode(Bucket *one_, Bucket *two_, int depth_);
+};
+
+BucketNode::BucketNode(Bucket *one_, Bucket *two_, int depth_)
+{
+	one = one_;
+	two = two_;
+	depth = depth_;
+}
+
+
+BucketNode* decantBucket1(BucketNode *Parent)
+{
+	cout << "decant1" << endl;
+	Bucket one = *(*Parent).one;
+	Bucket two = *(*Parent).two;
+	one.decantBucket(two);
+	BucketNode current(&one, &two, (*Parent).depth + 1);
+	mainLoop(current);
+	return &current;
+}
+
+BucketNode* decantBucket2(BucketNode *Parent)
+{
+	Bucket one = *(*Parent).one;
+	Bucket two = *(*Parent).two;
+	two.decantBucket(one);
+	BucketNode current(&one, &two, (*Parent).depth + 1);
+	mainLoop(current);
+	return &current;
+}
+
+BucketNode* emptyBucket1(BucketNode *Parent)
+{
+	Bucket one = *(*Parent).one;
+	one.emptyBucket();
+	BucketNode current(&one, (*Parent).two, (*Parent).depth + 1);
+	mainLoop(current);
+	return &current;
+}
+
+BucketNode* emptyBucket2(BucketNode *Parent)
+{
+	Bucket two = *(*Parent).two;
+	two.emptyBucket();
+	BucketNode current((*Parent).one, &two, (*Parent).depth + 1);
+	mainLoop(current);
+	return &current;
+}
+
+BucketNode* fillBucket1(BucketNode *Parent)
+{
+	Bucket one = *(*Parent).one;
+	one.fillBucket();
+	BucketNode current(&one, (*Parent).two, (*Parent).depth + 1);
+	mainLoop(current);
+	return &current;
+}
+
+BucketNode* fillBucket2(BucketNode *Parent)
+{
+	Bucket two = *(*Parent).two;
+	two.fillBucket();
+	BucketNode current((*Parent).one, &two, (*Parent).depth + 1);
+	mainLoop(current);
+	return &current;
+}
+
+int maxDepth, goal;
+bool finished = false;
+
+void mainLoop(BucketNode CurrentBucketNode)
+{
+	if (CurrentBucketNode.depth > maxDepth || finished)
+		return;
+
+	Bucket one = *(CurrentBucketNode.one);
+	Bucket two = *(CurrentBucketNode.two);
+
+	if (one.curVol == goal || two.curVol == goal)
+	{
+		cout << "Found solution! depth=" << CurrentBucketNode.depth << endl;
+		one.print();
+		one.print();
+		finished = true;
+		return;
+	}
+
+
+	if (!finished && one.curVol > 0 && two.curVol != two.maxVol)
+	{
+		CurrentBucketNode.decant1 = decantBucket1(&CurrentBucketNode);
+	}
+	if (!finished && two.curVol > 0 && one.curVol != two.maxVol)
+	{
+		CurrentBucketNode.decant2 = decantBucket2(&CurrentBucketNode);
+	}
+	if (!finished && one.curVol > 0)
+	{
+		CurrentBucketNode.empty1 = emptyBucket1(&CurrentBucketNode);
+	}
+	if (!finished && two.curVol > 0)
+	{
+		CurrentBucketNode.empty2 = emptyBucket2(&CurrentBucketNode);
+	}
+	if (!finished && one.curVol < one.maxVol)
+	{
+		CurrentBucketNode.fill1 = fillBucket1(&CurrentBucketNode);
+	}
+	if (!finished && two.curVol < two.maxVol)
+	{
+		CurrentBucketNode.fill2 = fillBucket2(&CurrentBucketNode);
+	}
+}
 
 
 int main()
 {
-    return 0;
+	int maxVol1, maxVol2;
+	cout << "Specify the maximum Volume of the first Bucket: ";
+	cin >> maxVol1;
+	cout << "Specify the maximum Volume of the second Bucket: ";
+	cin >> maxVol2;
+	cout << "Specify the Goal: ";
+	cin >> goal;
+	cout << "Specify the maximum search depth: ";
+	cin >> maxDepth;
+
+	if (goal > maxVol1 && goal > maxVol2)
+	{
+		cout << "Goal cannot be bigger than the largest Bucket!";
+		return -1;
+	}	
+    
+	Bucket Bucket1(maxVol1, 0, "Bucket1");
+	Bucket Bucket2(maxVol2, 0, "Bucket2");
+
+	BucketNode Root(&Bucket1, &Bucket2, 0);
+
+	Bucket1.print();
+	Bucket2.print();
+
+	system("PAUSE");
+
+	mainLoop(Root);
+
+	system("PAUSE");
 }
 
---
+++ BruteBucket.h
@@ -0,0 +1,1 @@
+void mainLoop(BucketNode CurrentBucketNode);
